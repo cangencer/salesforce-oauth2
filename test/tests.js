3b782b420327363b577f9f2d45470422b9d0003c
Can Gencer <cgencer@gmail.com>
2012-03-23 12:23:24 +0000

    add support for username/password flow

--- test/tests.js
+++ test/tests.js
@@ -25,9 +25,10 @@ describe("tokens", function () {
         access_token:"00Dd0000000dsWL!AR8AQCKKVxOwRhqhwXqNthdufggKWdUOOrp866CrJeEqF41eYP1kxtYmLMGxTkfRjFbzsD.Aqh8wvDyKyOPAVrDuyJS_bh2."
     };
 
-    describe("when getting the access token", function () {
+    describe("when asking for an access token", function () {
         var expectedPath = "/services/oauth2/token?grant_type=authorization_code&redirect_uri=http%3A%2F%2Fsome%2Fcallback%2Furl&client_id=some%20consumer%20key&client_secret=some%20consumer%20secret&code=some%20authorization%20code",
             options = {
+                grant_type: "authorization_code",
                 redirect_uri:"http://some/callback/url",
                 client_id:"some consumer key",
                 client_secret:"some consumer secret",
@@ -77,6 +78,56 @@ describe("tokens", function () {
         });
     });
 
+    describe("when authenticating with an authorization code", function() {
+        options = {                
+                redirect_uri:"http://some/callback/url",
+                client_id:"some consumer key",
+                client_secret:"some consumer secret",
+                code:"some authorization code"
+        };
+
+        var expectedPath = "/services/oauth2/token?grant_type=authorization_code&client_id=some%20consumer%20key&client_secret=some%20consumer%20secret&refresh_token=some%20refresh%20token",
+            options = {
+                client_id:"some consumer key",
+                client_secret:"some consumer secret",
+                refresh_token:"some refresh token"
+            };
+
+        it("should parse and verify the response", function (done) {
+            var login = nock("https://login.salesforce.com")
+                .post(expectedPath)
+                .reply(200, mockResponse);
+
+            oauth2.authenticate(options, function (error, payload) {
+                should.not.exist(error);
+                payload.should.eql(mockResponse);
+                login.done();
+                done();
+            });
+        });
+    });
+    describe("when authenticating with username and password", function () {
+        var expectedPath = "/services/oauth2/token?grant_type=password&client_id=some%20consumer%20key&client_secret=some%20consumer%20secret&username=some%20username&password=some%20password",
+            options = {
+                client_id:"some consumer key",
+                client_secret:"some consumer secret",
+                username: "some username",
+                password: "some password"
+            };
+
+        it("should parse and verify the response", function (done) {
+            var login = nock("https://login.salesforce.com")
+                .post(expectedPath)
+                .reply(200, mockResponse);
+
+            oauth2.password(options, function (error, payload) {
+                should.not.exist(error);
+                payload.should.eql(mockResponse);
+                login.done();
+                done();
+            });
+        });
+    });
     describe("when getting the refresh token", function () {
         var expectedPath = "/services/oauth2/token?grant_type=refresh_token&client_id=some%20consumer%20key&client_secret=some%20consumer%20secret&refresh_token=some%20refresh%20token",
             options = {
@@ -93,35 +144,6 @@ describe("tokens", function () {
             oauth2.refresh(options, function (error, payload) {
                 should.not.exist(error);
                 payload.should.eql(mockResponse);
-                login.done();
-                done();
-            });
-        });
-
-        it("should have error if http status code > 400", function (done) {
-            var login = nock("https://login.salesforce.com")
-                .post(expectedPath)
-                .reply(401, "");
-
-            oauth2.refresh(options, function (error, payload) {
-                should.not.exist(payload);
-                should.exist(error);
-                login.done();
-                done();
-            });
-        });
-
-        it("should have error if signature is invalid", function (done) {
-            var invalidResponse = _.clone(mockResponse);
-            invalidResponse.signature = "invalid signature";
-
-            var login = nock("https://login.salesforce.com")
-                .post(expectedPath)
-                .reply(200, invalidResponse);
-
-            oauth2.refresh(options, function (error, payload) {
-                should.exist(error);
-                should.not.exist(payload);
                 login.done();
                 done();
             });
